c504205bd68e9633dde1890871e2008b98a385ce
torvic <130111@unsaac.edu.pe>
2021-05-13 17:40:06 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Componente from './components/Componente'
+import Propiedades from './components/Propiedades'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -29,11 +31,22 @@ function App() {
         >
           Learning React
         </a>
-      </header>
+        </section>
       <section>
         <Componente msg="Hola soy un componente funcional expresada desde una prop"/>
+        <hr/>
+        <Propiedades 
+        cadena="Esto es una cadena de Texto" 
+        numero= {19}
+        booleano={false} 
+        arreglo={[1,2,3]} 
+        objeto={{nombre: "Jon", correo:"boom@gmail.com"}} 
+        funcion={num => num*num}
+        elementoReact={<i>Este es un elemento React</i>} 
+        componenteReact={<Componente msg="Soy un componente basado como prop"/>}
+        />
       </section>
-      
+      </header>
     </div>
   );
 }
