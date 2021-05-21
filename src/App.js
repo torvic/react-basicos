fbf45f4f605561a7eb130e6ebdfeb4fd9f1c7094
torvic <130111@unsaac.edu.pe>
2021-05-21 18:10:16 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {EventosES6, EventosES7, MasSobreEventos} from './components/Eventos';
 import ComunicacionComponente from './components/ComunicacionComponente';
 import CicloVida from './components/CicloVida';
 import AjaxApis from './components/AjaxApis';
+import ContadorHooks from './components/ContadorHooks'
 
 function App() {
   let personas = ["Alfonso", "Ugarte", "San", "mArtin"];
@@ -70,6 +71,8 @@ function App() {
         <CicloVida/>
         <hr/>
         <AjaxApis/>
+        <hr/>
+        <ContadorHooks titulo="Seguidores"/>
       </section>
       </header>
     </div>
---
+++ src/components/ContadorHooks.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+export default function ContadorHooks(props) {
+  //console.log(useState());
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => setContador(contador+1);
+  const restar = () => setContador(contador-1);
+
+  return(
+    <>
+      <h2>Hooks - UseState</h2>
+      <nav>
+        <button onClick={sumar}> + </button>
+        <button onClick={restar}> - </button>
+      </nav>
+      <p>Contador de {props.titulo}</p>
+      <h3>{contador}</h3>
+    </>
+  );
+}
+
+ContadorHooks.defaultProps = {
+  titulo: "Clicks",
+};
