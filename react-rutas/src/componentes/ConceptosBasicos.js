17627beb376a288f1c3a3b0d9f34b9e07ee9e379
torvic <130111@unsaac.edu.pe>
2021-06-18 17:31:48 -0500

    nav y navlink

--- react-rutas/src/componentes/ConceptosBasicos.js
+++ react-rutas/src/componentes/ConceptosBasicos.js
@@ -2,8 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
+import Error404 from "../pages/Error404";
+import Home from "../pages/Home";
+import MenuConceptos from "./MenuConceptos";
 
 const ConceptosBasicos = () => {
+  return (
+    <div>
+      <h2>Conceptos Basicos</h2>
+      <Router>
+				<MenuConceptos/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/acerca" component={Acerca} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+/* const ConceptosBasicos = () => {
   return (
     <div>
       <h2>Conceptos Basicos</h2>
@@ -22,7 +42,7 @@ const ConceptosBasicos = () => {
               harum ullam maxime eum ipsa!
             </p>
           </Route>
-          {/* <Route exact path="/contacto" component={Contacto} /> */}
+          <Route exact path="/contacto" component={Contacto} />
           <Route
             exact
             path="/contacto"
@@ -43,6 +63,6 @@ const ConceptosBasicos = () => {
       </Router>
     </div>
   );
-};
+}; */
 
 export default ConceptosBasicos;
--- react-rutas/src/pages/Acerca.js
+++ react-rutas/src/pages/Acerca.js
@@ -2,6 +2,7 @@ const Acerca = () => {
   return (
     <div>
       <h2>Acerca</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, expedita inventore? Eum aut sequi voluptates, laboriosam velit fuga odio nulla at reprehenderit voluptatum ipsa nobis libero rem consectetur, iusto ea?</p>
     </div>
   );
 };
